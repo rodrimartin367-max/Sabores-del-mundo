43f4d55d1536750c12a7f3522d012128b4731329
Rodrimartin <rodrimartin367@gmail.com>
2026-05-12 21:51:22 +0200

    Credenciales de Railway actualizadas

--- src/main/java/util/DBUtil.java
+++ src/main/java/util/DBUtil.java
@@ -2,14 +2,13 @@ package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DBUtil {
-    private static final String URL = "jdbc:mysql://db:3306/sabores_del_mundo?allowPublicKeyRetrieval=true&useSSL=false&useUnicode=true&characterEncoding=UTF-8";
+    private static final String URL = "jdbc:mysql://mysql.railway.internal:3306/railway?allowPublicKeyRetrieval=true&useSSL=false&useUnicode=true&characterEncoding=UTF-8";
     private static final String USER = "root";
-    private static final String PASS = "madrid";
+    private static final String PASS = "XlZSsyGNDAOpsdGBogOJXSqwhuJzAbMb";
 
-    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+    public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(URL, USER, PASS);
     }
